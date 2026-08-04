877455aaf1247710f6f99204cbe991f04773c369
Zahra Moneeb <zahramoneeb@gmail.com>
2026-08-04 23:29:15 +0430

    fix production create opportunity

--- src/app/api/opportunities/route.ts
+++ src/app/api/opportunities/route.ts
@@ -22,55 +22,19 @@ export async function GET() {
 
 
 // POST create opportunity
+
 export async function POST(request: Request) {
-
   try {
-
     const newOpportunity = await request.json();
 
-
-    const fileData = fs.readFileSync(
-      filePath,
-      "utf-8"
-    );
-
-
-    const opportunities: Opportunity[] =
-      JSON.parse(fileData);
-
-
-    const newId =
-      opportunities.length > 0
-        ? Math.max(
-            ...opportunities.map((item) => item.id)
-          ) + 1
-        : 1;
-
+    const newId = Date.now();
 
     const opportunityToAdd = {
       id: newId,
-      postedAt: new Date()
-        .toISOString()
-        .split("T")[0],
-
+      postedAt: new Date().toISOString().split("T")[0],
       featured: false,
-
       ...newOpportunity,
     };
-
-
-    opportunities.push(opportunityToAdd);
-
-
-    fs.writeFileSync(
-      filePath,
-      JSON.stringify(
-        opportunities,
-        null,
-        2
-      )
-    );
-
 
     return NextResponse.json(
       opportunityToAdd,
@@ -79,18 +43,16 @@ export async function POST(request: Request) {
       }
     );
 
-
   } catch (error) {
-  console.error("POST Error:", error);
+    console.error("POST Error:", error);
 
-  return NextResponse.json(
-    {
-      message: "Failed to create opportunity",
-      error: String(error),
-    },
-    {
-      status: 500,
-    }
-  );
-}
+    return NextResponse.json(
+      {
+        message: "Failed to create opportunity",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
 }
